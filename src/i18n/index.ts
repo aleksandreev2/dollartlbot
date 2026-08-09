aabd2d955890a0ecd8cc9e012393d18fe8cc2c7b
aleksandreev2 <sashahumortele3@gmail.com>
2026-08-09 15:15:00 +0400

    Apply localized regular status labels

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -12,20 +12,21 @@ import { ru } from './ru';
 import { featureTranslations } from './features';
 import { policyTranslations } from './policy';
 import { policyOverrideTranslations } from './policy_override';
+import { statusOverrideTranslations } from './status_override';
 
 export { SUPPORTED_LANGUAGES, type Locale } from './types';
 
 const dictionaries = {
-  en: { ...en, ...featureTranslations.en, ...policyTranslations.en, ...policyOverrideTranslations.en },
-  es: { ...es, ...featureTranslations.es, ...policyTranslations.es, ...policyOverrideTranslations.es },
-  fil: { ...fil, ...featureTranslations.fil, ...policyTranslations.fil, ...policyOverrideTranslations.fil },
-  hi: { ...hi, ...featureTranslations.hi, ...policyTranslations.hi, ...policyOverrideTranslations.hi },
-  pt: { ...pt, ...featureTranslations.pt, ...policyTranslations.pt, ...policyOverrideTranslations.pt },
-  id: { ...id, ...featureTranslations.id, ...policyTranslations.id, ...policyOverrideTranslations.id },
-  vi: { ...vi, ...featureTranslations.vi, ...policyTranslations.vi, ...policyOverrideTranslations.vi },
-  fr: { ...fr, ...featureTranslations.fr, ...policyTranslations.fr, ...policyOverrideTranslations.fr },
-  de: { ...de, ...featureTranslations.de, ...policyTranslations.de, ...policyOverrideTranslations.de },
-  ru: { ...ru, ...featureTranslations.ru, ...policyTranslations.ru, ...policyOverrideTranslations.ru },
+  en: { ...en, ...featureTranslations.en, ...policyTranslations.en, ...policyOverrideTranslations.en, ...statusOverrideTranslations.en },
+  es: { ...es, ...featureTranslations.es, ...policyTranslations.es, ...policyOverrideTranslations.es, ...statusOverrideTranslations.es },
+  fil: { ...fil, ...featureTranslations.fil, ...policyTranslations.fil, ...policyOverrideTranslations.fil, ...statusOverrideTranslations.fil },
+  hi: { ...hi, ...featureTranslations.hi, ...policyTranslations.hi, ...policyOverrideTranslations.hi, ...statusOverrideTranslations.hi },
+  pt: { ...pt, ...featureTranslations.pt, ...policyTranslations.pt, ...policyOverrideTranslations.pt, ...statusOverrideTranslations.pt },
+  id: { ...id, ...featureTranslations.id, ...policyTranslations.id, ...policyOverrideTranslations.id, ...statusOverrideTranslations.id },
+  vi: { ...vi, ...featureTranslations.vi, ...policyTranslations.vi, ...policyOverrideTranslations.vi, ...statusOverrideTranslations.vi },
+  fr: { ...fr, ...featureTranslations.fr, ...policyTranslations.fr, ...policyOverrideTranslations.fr, ...statusOverrideTranslations.fr },
+  de: { ...de, ...featureTranslations.de, ...policyTranslations.de, ...policyOverrideTranslations.de, ...statusOverrideTranslations.de },
+  ru: { ...ru, ...featureTranslations.ru, ...policyTranslations.ru, ...policyOverrideTranslations.ru, ...statusOverrideTranslations.ru },
 } as const;
 
 export type TranslationKey = keyof typeof dictionaries.en;
