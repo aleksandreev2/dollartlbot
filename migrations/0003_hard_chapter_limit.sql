c2dbb4a1d31fe5f0781b51afde9ca1173ee31ea6
aleksandreev2 <sashahumortele3@gmail.com>
2026-08-09 15:14:29 +0400

    Scope 250 chapter database guard to regular users

--- migrations/0003_hard_chapter_limit.sql
+++ migrations/0003_hard_chapter_limit.sql
@@ -1,13 +1,13 @@
 CREATE TRIGGER IF NOT EXISTS submissions_max_250_insert
 BEFORE INSERT ON submissions
-WHEN NEW.chapter_count > 250
+WHEN NEW.plan = 'free' AND NEW.chapter_count > 250
 BEGIN
-  SELECT RAISE(ABORT, 'chapter_count_exceeds_250');
+  SELECT RAISE(ABORT, 'regular_chapter_count_exceeds_250');
 END;
 
 CREATE TRIGGER IF NOT EXISTS submissions_max_250_update
-BEFORE UPDATE OF chapter_count ON submissions
-WHEN NEW.chapter_count > 250
+BEFORE UPDATE OF chapter_count, plan ON submissions
+WHEN NEW.plan = 'free' AND NEW.chapter_count > 250
 BEGIN
-  SELECT RAISE(ABORT, 'chapter_count_exceeds_250');
+  SELECT RAISE(ABORT, 'regular_chapter_count_exceeds_250');
 END;
